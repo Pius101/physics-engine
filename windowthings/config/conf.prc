e1e5e5b98002ab686520b9c68ab6d949240d145e
pumpkins <116141356+Pius101@users.noreply.github.com>
2026-06-27 22:38:37 +0800

    forgot to stage some things

--- windowthings/config/conf.prc
+++ windowthings/config/conf.prc
@@ -1,3 +1,4 @@
 win-size 1280 720
 show-frame-rate-meter True
 load-file-type p3assimp
+window-title physics engine 
--- windowthings/scene.py
+++ windowthings/scene.py
@@ -8,14 +8,15 @@ from panda3d.core import Filename
 class MyApp(ShowBase):
     def __init__(self):
         super().__init__()
-        winfile = "E:\\pythongeneral\\physics engine -me\\object\\95x2swyz2fls-wheel\\treckter.obj"
+        winfile = "E:\\pythongeneral\\physics engine -me\\model\\treckter.obj"
         pandafile = Filename.fromOsSpecific(winfile)
         print(pandafile)
-        #self.disableMouse()
+        self.disableMouse()
         
         #self.set_background_color(g=1,b=1,r=1,a=1)
-        model= self.loader.loadModel(pandafile) 
-        
+        model= self.loader.loadModel(pandafile)
+
+
         model.set_pos(0,10,0)
         model.reparentTo(self.render)    
 
